b1c7b4adf9a32b7ef6f00bc41052f93077002bbf
Jasmin68k <50185302+Jasmin68k@users.noreply.github.com>
2021-05-02 20:04:10 +0200

    Added example for mouse event (clickable rectangle)

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -7,6 +7,17 @@ import P5 from 'p5' // Package from npm
 
 export default {
   name: 'Canvas',
+
+  data() {
+    return {
+      rectX: 200,
+      rectY: 200,
+      rectW: 150,
+      rectH: 75,
+      bgColor: 50
+    }
+  },
+
   props: {
     moo: String
   },
@@ -18,8 +29,20 @@ export default {
       }
       p5.draw = () => {
         p5.clear()
+        p5.background(this.bgColor)
         p5.ellipse(p5.mouseX, p5.mouseY, 40, 40)
         p5.text(this.moo, 50, 50)
+        p5.rect(this.rectX, this.rectY, this.rectW, this.rectH)
+      }
+      p5.mousePressed = () => {
+        if (
+          p5.mouseX >= this.rectX &&
+          p5.mouseX <= this.rectX + this.rectW &&
+          p5.mouseY >= this.rectY &&
+          p5.mouseY <= this.rectY + this.rectH
+        ) {
+          this.bgColor = p5.random(255)
+        }
       }
     }
 
