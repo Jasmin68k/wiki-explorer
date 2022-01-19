ba3d454cbc584472e763c60ddb6a8a40ceb83acb
Jasmin68k <50185302+Jasmin68k@users.noreply.github.com>
2022-01-19 20:53:39 +0100

    eslint config fix for class fields (necessary after earlier removal of babel in VueCLI to Vite switch).

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
-    es2021: true
+    node: true
   },
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  parserOptions: { ecmaVersion: 'latest' },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
