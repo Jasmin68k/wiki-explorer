92bfd359f0e4a173763b983fd1eb9c660c702a8d
Jasmin68k <50185302+Jasmin68k@users.noreply.github.com>
2022-03-25 23:15:41 +0100

    Added categories/redirects in MainInfo view.

---
+++ src/components/CategoriesRedirectsTitle.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    v-if="global.statefull.titlePage.categories.length > 0"
+    class="catsredirects"
+  >
+    <span
+      ><b>{{ $t('categories') }}</b></span
+    >
+
+    <ul class="list-horizontal">
+      <li
+        v-for="category in global.statefull.titlePage.categories"
+        :key="category"
+      >
+        {{ category }}
+      </li>
+    </ul>
+  </div>
+
+  <div
+    v-if="global.statefull.titlePage.redirects.length > 0"
+    class="catsredirects"
+  >
+    <span
+      ><b>{{ $t('redirects') }}</b></span
+    >
+    <ul class="list-horizontal">
+      <li
+        v-for="redirect in global.statefull.titlePage.redirects"
+        :key="redirect"
+      >
+        {{ redirect }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { inject } from 'vue'
+export default {
+  name: 'CategoriesRedirectsTitle',
+  setup() {
+    const global = inject('global')
+    return { global }
+  }
+}
+</script>
+<style scoped>
+.catsredirects {
+  font-size: 90%;
+  text-align: left;
+  padding-right: 10px;
+  padding-left: 5px;
+}
+
+ul {
+  padding: 0;
+}
+.list-horizontal li {
+  display: inline-block;
+}
+.list-horizontal li:before {
+  content: '\00a0\2022\00a0';
+}
+
+/* .list-horizontal li:first-child:before {
+  content: '';
+} */
+</style>
--- src/components/MainTitleInfo.vue
+++ src/components/MainTitleInfo.vue
@@ -1,41 +1,65 @@
 <template>
   <div>
     <div
-      v-show="
+      v-if="
         global.statefull.titlePage.extract && global.statefull.titlePage.image
       "
     >
       <div>
         <img class="image" :src="global.statefull.titlePage.image" />
       </div>
+      <div v-if="global.statefull.titlePage.title">
+        <h2>
+          {{ global.statefull.titlePage.title }}
+        </h2>
+      </div>
+
       <div class="extract" v-html="global.statefull.titlePage.extract"></div>
+      <CategoriesRedirectsTitle></CategoriesRedirectsTitle>
     </div>
     <div
-      v-show="
+      v-if="
         global.statefull.titlePage.extract && !global.statefull.titlePage.image
       "
     >
+      <div v-if="global.statefull.titlePage.title">
+        <h2>
+          {{ global.statefull.titlePage.title }}
+        </h2>
+      </div>
+
       <div class="extract" v-html="global.statefull.titlePage.extract"></div>
+      <CategoriesRedirectsTitle></CategoriesRedirectsTitle>
     </div>
     <div
-      v-show="
+      v-if="
         !global.statefull.titlePage.extract && global.statefull.titlePage.image
       "
     >
+      <div v-if="global.statefull.titlePage.title">
+        <h2>
+          {{ global.statefull.titlePage.title }}
+        </h2>
+      </div>
+
       <div>
         <img class="imageonly" :src="global.statefull.titlePage.image" />
       </div>
+      <CategoriesRedirectsTitle></CategoriesRedirectsTitle>
     </div>
   </div>
 </template>
 <script>
 import { inject } from 'vue'
+import CategoriesRedirectsTitle from './CategoriesRedirectsTitle.vue'
+
 export default {
   name: 'MainTitleInfo',
   setup() {
     const global = inject('global')
     return { global }
-  }
+  },
+  components: { CategoriesRedirectsTitle }
 }
 </script>
 <style scoped>
